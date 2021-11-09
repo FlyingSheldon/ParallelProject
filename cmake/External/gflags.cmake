6504677c8df433f78e78b8beeb6b3190c27f8fb1
huangjiahua <jh@jiahuah.com>
2021-11-08 19:29:29 -0500

    change the version of gflags

--- cmake/External/gflags.cmake
+++ cmake/External/gflags.cmake
@@ -23,10 +23,10 @@ if (NOT __GFLAGS_INCLUDED) # guard against multiple includes
     set(GFLAGS_CXX_FLAGS ${CMAKE_CXX_FLAGS} ${GFLAGS_EXTRA_COMPILER_FLAGS})
     set(GFLAGS_C_FLAGS ${CMAKE_C_FLAGS} ${GFLAGS_EXTRA_COMPILER_FLAGS})
 
-    ExternalProject_Add(gflags
+    ExternalProject_Add(gflags-static
       PREFIX ${gflags_PREFIX}
       GIT_REPOSITORY "https://github.com/gflags/gflags.git"
-      GIT_TAG "v2.1.2"
+      GIT_TAG "v2.2.2"
       UPDATE_COMMAND ""
       INSTALL_DIR ${gflags_INSTALL}
       CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
@@ -35,7 +35,6 @@ if (NOT __GFLAGS_INCLUDED) # guard against multiple includes
                  -DBUILD_STATIC_LIBS=ON
                  -DBUILD_PACKAGING=OFF
                  -DBUILD_TESTING=OFF
-                 -DBUILD_CONFIG_TESTS=OFF
                  -DBUILD_NC_TESTS=OFF
                  -DINSTALL_HEADERS=ON
                  -DCMAKE_C_FLAGS=${GFLAGS_C_FLAGS}

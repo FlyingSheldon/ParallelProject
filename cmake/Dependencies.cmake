1db51c4798d02fc7507ab3d9c85c7dcb83e1fabc
Jiahua Huang <jiahuah@andrew.cmu.edu>
2021-11-23 21:42:45 -0500

    setup cuda in cmake

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -13,8 +13,12 @@ list(APPEND pp_LINKER_LIBS PUBLIC ${GFLAGS_LIBRARIES})
 
 # ---[ Halide
 find_package(LLVM REQUIRED CONFIG PATHS /usr/local/Cellar/)
-find_package(Halide REQUIRED)
-list(APPEND pp_LINKER_LIBS PRIVATE Halide::Halide)
+find_package(Halide QUIET)
+if( Halide_FOUND )
+    list(APPEND pp_LINKER_LIBS PRIVATE Halide::Halide)
+else()
+    message("Halide is not found on this system")
+endif()
 
 # ---[ JPEG
 find_package(JPEG)
